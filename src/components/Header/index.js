35c7913517fdcef81de0aa968c9d23f352cf643d
Luiz Fernando Cardozo <luizfcardozo@gmail.com>
2020-02-25 11:15:49 -0300

    Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logoPurple from '~/assets/logo_purple.svg';
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logoPurple} alt="GoBarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Meu nome</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Nome"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
