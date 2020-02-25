b6b01917210b9452e8235820f3609efdf4fb15af
Luiz Fernando Cardozo <luizfcardozo@gmail.com>
2020-02-25 10:28:09 -0300

    Add toast notification

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import api from '~/services/api';
 import history from '~/services/history';
 import { signInSuccess, signFailure } from './actions';
@@ -14,7 +15,7 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('Usuário não é provider');
+      toast.error('Usuário não é provider');
       return;
     }
 
@@ -22,6 +23,7 @@ export function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Falha na autenticação, verifique seus dados');
     yield put(signFailure());
   }
 }
