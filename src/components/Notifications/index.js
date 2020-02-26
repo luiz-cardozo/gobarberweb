f224bb39a01547604a0589e359160744cf9c6045
Luiz Fernando Cardozo <luizfcardozo@gmail.com>
2020-02-26 13:36:41 -0300

    Update avatar on profile page

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import { MdNotifications } from 'react-icons/md';
-import { parseISO, formatDistance } from 'date-fns';
+import { parseISO, formatDistance, formatDistanceToNow } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 
 import api from '~/services/api';
@@ -25,11 +25,10 @@ export default function Notifications() {
       const response = await api.get('notifications');
       const data = response.data.map(notification => ({
         ...notification,
-        timeDistance: formatDistance(
-          parseISO(notification.createdAt),
-          new Date(),
-          { addSuffix: true, locale: pt }
-        ),
+        timeDistance: formatDistanceToNow(parseISO(notification.createdAt), {
+          addSuffix: true,
+          locale: pt,
+        }),
       }));
       setNotifications(data);
     }
